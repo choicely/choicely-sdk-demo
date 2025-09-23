010b45b04fb8798d56b2f60a1930283d88565d70
Oleksandr Kryvodub <oleksandrkryvodub@gmail.com>
2025-09-23 14:45:23 +0300

    ChoicelyCore added to iOS demo app

--- iOS/sdk-ios-demo/DemoApp.swift
+++ iOS/sdk-ios-demo/DemoApp.swift
@@ -6,12 +6,21 @@
 //
 
 import SwiftUI
+import ChoicelyCore
 
 @main
 struct DemoApp: App {
+    
+    init() {
+        ChoicelySDK.initialize(
+            application: UIApplication.shared,
+            appKey: "Y2hvaWNlbHktZXUvYXBwcy9kS1lHUUtUbWREa1pRb1ltZFRiZQ"
+        )
+    }
+    
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ChoicelySplashView()
         }
     }
 }

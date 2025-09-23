44e2dac98ca0223ce40443b93383c555a91446ed
Oleksandr Kryvodub <oleksandrkryvodub@gmail.com>
2025-09-23 14:43:03 +0300

    Basic iOS app setup

---
+++ iOS/sdk-ios-demo/DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoApp.swift
+//  sdk-ios-demo
+//
+//  Created by Oleksandr Kryvodub on 23.9.2025.
+//
+
+import SwiftUI
+
+@main
+struct DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
